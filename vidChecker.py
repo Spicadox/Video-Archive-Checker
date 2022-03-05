788d57cdc03c2e9d371dd7afc9c1a1d0dab45ddd
Sam Phung <sphung@uncc.edu>
2022-03-04 23:53:34 -0500

    Fixed issue

--- vidChecker.py
+++ vidChecker.py
@@ -17,8 +17,8 @@ def checkArguments():
 def directoryChecker(directories):
     totalCount = 0
     videoCount = 0
-
-    extension_list = [".mkv", ".mp4", ".webm"]
+    previousFile = ""
+    extension_list = [".mkv", ".mp4", ".webm", ".ts"]
     with open("output.csv", 'w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
         for directory in directories:
@@ -26,30 +26,31 @@ def directoryChecker(directories):
                 totalCount += len(dirs)
                 if len(files) == 0:
                     if len(dirs) != 0:
-                        totalCount+=1
+                        totalCount += 1
                         print("Parent Directory: " + roots)
+                    # Does nothing?
                     else:
+                        # print("\t" + roots)
+                        # csv_writer.writerow([roots])
                         videoCount = videoCount + 1
-                        print("\t"+roots)
-                        csv_writer.writerow([roots])
+                        writeDir = files[0:8] + "-" + roots
+                        print("\t" + writeDir)
+                        csv_writer.writerow([writeDir])
                 # Loop through all files in the subfolders in reverse order
                 for file in reversed(files):
                     # If any of the files is a video then increment videoCount and set fileFound to true
                     # and go to next folder
                     if extension_list[0] in file or extension_list[1] in file or extension_list[2] in file:
-                        # videoCount = videoCount + 1
+                        previousFile = file
                         break
                     # Else if there aren't any videos in the folder write it and all files have been seen
-                    else:
-                        #len(dirs) == 0 solved and prevented user input directory to be considered as empty
-                        #Doesnt do anything
-
-                        if file == files[0] and len(dirs) == 0:
-                            # testing += 1
-                            videoCount+=1
-                            print("\t"+roots)
-                            csv_writer.writerow([roots])
-                            break
+                    # Get the date of the dir(more accurately the file date) along with the name of the directory
+                    elif file == files[0] and len(dirs) == 0:
+                        videoCount += 1
+                        writeDir = previousFile[0:8] + " - " + roots
+                        print("\t" + writeDir)
+                        csv_writer.writerow([writeDir])
+                        break
 
     print("Total Directories: " + str(totalCount))
     print("Missing Videos: " + str(videoCount))
